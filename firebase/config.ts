8f98139a60150c0d8cc14b220a67f591d5eb4836
Ivan Aguilar <aguilar.zuniga.ivan@gmail.com>
2026-05-11 22:58:44 -0600

    Fixed Code

--- firebase/config.ts
+++ firebase/config.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { ReactNativeAsyncStorage } from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "taa01-eab87.firebaseapp.com",
-  projectId: "taa01-eab87",
-  storageBucket: "taa01-eab87.firebasestorage.app",
-  messagingSenderId: "778760395898",
-  appId: "1:778760395898:web:97ce71b9b02ede27f36fc3",
-  measurementId: "G-DXJHW6R4NN",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
